73c24bb69232161e2bdf15f6c773fb23f59db30c
木佐凪樹 <lichddd@126.com>
2015-09-11 16:03:48 +0800

    1111

--- js/ImgChange.js
+++ js/ImgChange.js
@@ -27,16 +27,28 @@ if ("undefined" == typeof jQuery) throw new Error("Bootstrap's JavaScript requir
   var ImgChanger   = function (el,option) {
 //  $(el).on('click',$.proxy(this.mychange,this));
     this.$el=$(el);
-    this.$eel=$('<div style="width:100%;height:100%"></div>').appendTo(this.$el);
-    this.$progress=$('<div style="width:0%;height:2px;background-color:#0086B3"></div>').appendTo(this.$el);
+    this.$eel=$('<div class="imgchanger_continer" style="width:100%;height:100%"></div>').appendTo(this.$el);
+    this.$progress=$('<div class="imgchanger_progress" style="width:0%;height:2px;background-color:#0086B3"></div>').appendTo(this.$el);
+    this.$pager=$('<div class="imgchanger_pager"></div>').appendTo(this.$el);
     this.imgindex=0;
     this.preimgindex=-1;
     this.imgarr=[];
 	this.arr=[];
 	this.houkou="migi";
+	this.changetime=5000;
     $.extend(true, this, option);
+    for (var i = 0; i < this.imgarr.length; i++) {
+    	this.$pager.append("<div style='width: "+ 90/this.imgarr.length +"%;margin:"+5/this.imgarr.length+"%'></div>");
+    }
+    self=this;
+    this.$pager.children('div').on('click',function () {
+    	
+    	self.jumpin(self.$pager.children('div').index($(this)));
+    	
+    })
     $(el).css('background-image',"url('" + (this.imgarr[0]) +"')");
-
+	$(this.$el.children('.imgchanger_pager').children('div')[0]).css('box-shadow','0px 0px 4px 5px rgba(255,255,255,0.5)');
+	
 	this.run();
 
   }
@@ -44,17 +56,20 @@ if ("undefined" == typeof jQuery) throw new Error("Bootstrap's JavaScript requir
 	
 	
 	ImgChanger.prototype.run = function() {
-		this.$progress.animate({width:'100%'},5000);
 		self=this;
+		this.$progress.animate({width:'100%'},this.changetime,function () {
+			self.$progress.css('width','0%');
+//			self.$progress.animate({width:'0%'},0);
+
+		});
+		
 		setTimeout(function () {
-			setTimeout(function () {
-			self.$progress.animate({width:'0%'},0);
-		},500)
+
 			
 			self.mychange();
 			
 			self.run();
-		},5000)
+		},this.changetime)
 		
 	}
 	
@@ -74,6 +89,69 @@ if ("undefined" == typeof jQuery) throw new Error("Bootstrap's JavaScript requir
 
 
 		
+	}
+	
+	ImgChanger.prototype.jumpin = function (index) {
+		
+	
+	
+	
+	if (this.arr.length>0) {
+		return
+	}
+
+	this.houkou="migi"
+		this.$eel.html('<div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>');
+	
+	this.$eel.find('div').each(function (index,element) {
+		$(this).css('background-position-y',-$('#continer').height()*index/10);
+	});
+	this.preimgindex=this.imgindex;
+	this.imgindex=index;
+	
+	
+	this.$el.css('background-image',"url('" + (this.imgarr[this.imgindex]) +"')");
+	var self=this;
+		this.$eel.find('div').each(function (index,element) {
+			
+//		$(this).show();
+		
+		var temp=$(this);
+		temp.css('background-image',"url('" + (self.imgarr[self.preimgindex]) +"')");
+		temp.css('visibility','visible');
+//		temp.css('transition-property','transform,opacity');
+		setTimeout(function() {
+//			temp.animate({transform:'translateZ(300px)'},'500');
+			self.arr.push(temp);
+			temp.css('transform','translateZ(300px)');
+			console.log(temp);
+			
+			
+		}, index*50);
+		setTimeout(function() {
+//			temp.animate({opacity:'0'},250);
+			temp.css('opacity','0');
+			console.log(temp);
+			
+			
+		}, index*50+250);
+		setTimeout(function() {
+////			arr.shift();
+//			temp.css('visibility','hidden');
+////			temp.css('transition-property','');
+//			temp.css('opacity','1');
+//			temp.css('transform','');
+//			temp.css('background-image',"url('img/" + imgindex +".jpg')");
+			self.arr.shift();
+			
+		}, index*50+500);
+
+	});
+	
+	
+	this.$el.children('.imgchanger_pager').children('div').css('box-shadow','');
+	$(this.$el.children('.imgchanger_pager').children('div')[this.imgindex]).css('box-shadow','0px 0px 4px 5px rgba(255,255,255,0.5)');
+	
 	}
 
 
@@ -230,8 +308,8 @@ if ("undefined" == typeof jQuery) throw new Error("Bootstrap's JavaScript requir
 		
 	}
 	
-	
-	
+	this.$el.children('.imgchanger_pager').children('div').css('box-shadow','');
+	$(this.$el.children('.imgchanger_pager').children('div')[this.imgindex]).css('box-shadow','0px 0px 4px 5px rgba(255,255,255,0.5)');
 	
 }
 	
--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 $().ready(function () {
-	$('#continer').imgChanger({imgarr:['img/1.jpg','img/2.jpg','img/3.jpg','img/4.jpg']});
+	$('#continer').imgChanger({imgarr:['img/1.jpg','img/2.jpg','img/3.jpg','img/4.jpg','img/5.jpg','img/6.jpg','img/7.jpg','img/8.jpg']});
 	
 //	$('#continer').imgChanger('mychange');
 	
