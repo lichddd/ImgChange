4bd842a7844a7b585d480ddca42a9f949b09bc51
木佐凪樹 <lichddd@126.com>
2015-09-11 14:31:25 +0800

    111

--- js/ImgChange.js
+++ js/ImgChange.js
@@ -25,24 +25,56 @@ if ("undefined" == typeof jQuery) throw new Error("Bootstrap's JavaScript requir
 	
 	
   var ImgChanger   = function (el,option) {
-    $(el).on('click',$.proxy(this.mychange,this));
-        this.$el=$(el);
+//  $(el).on('click',$.proxy(this.mychange,this));
+    this.$el=$(el);
+    this.$eel=$('<div style="width:100%;height:100%"></div>').appendTo(this.$el);
+    this.$progress=$('<div style="width:0%;height:2px;background-color:#0086B3"></div>').appendTo(this.$el);
     this.imgindex=0;
     this.preimgindex=-1;
     this.imgarr=[];
 	this.arr=[];
+	this.houkou="migi";
     $.extend(true, this, option);
     $(el).css('background-image',"url('" + (this.imgarr[0]) +"')");
 
+	this.run();
 
   }
 	
 	
 	
-	
+	ImgChanger.prototype.run = function() {
+		this.$progress.animate({width:'100%'},5000);
+		self=this;
+		setTimeout(function () {
+			setTimeout(function () {
+			self.$progress.animate({width:'0%'},0);
+		},500)
+			
+			self.mychange();
+			
+			self.run();
+		},5000)
+		
+	}
 	
 
+	ImgChanger.prototype.pre = function() {
+		self=this;
+		this.houkou="hitari"
+			self.mychange();
 
+
+		
+	}
+	ImgChanger.prototype.next = function() {
+		self=this;
+		this.houkou="migi"
+			self.mychange();
+
+
+		
+	}
 
 
 	ImgChanger.prototype.mychange = function() {
@@ -58,25 +90,29 @@ if ("undefined" == typeof jQuery) throw new Error("Bootstrap's JavaScript requir
 	}
 	
 	
-	this.$el.html('<div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>');
 	
-	this.$el.find('div').each(function (index,element) {
+	
+	
+	
+	if (this.houkou=="migi") {
+		this.$eel.html('<div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>');
+	
+	this.$eel.find('div').each(function (index,element) {
 		$(this).css('background-position-y',-$('#continer').height()*index/10);
 	});
-	
-	
-	this.imgindex+=1;
+	this.preimgindex=this.imgindex;
+		this.imgindex+=1;
 	if (this.imgindex>=this.imgarr.length) {
 		this.imgindex=0;
 	}
-	this.preimgindex+=1;
+	
 	if (this.preimgindex>=this.imgarr.length) {
 		this.preimgindex=0;
 	}
 	
 	this.$el.css('background-image',"url('" + (this.imgarr[this.imgindex]) +"')");
 	var self=this;
-		this.$el.find('div').each(function (index,element) {
+		this.$eel.find('div').each(function (index,element) {
 			
 //		$(this).show();
 		
@@ -111,6 +147,90 @@ if ("undefined" == typeof jQuery) throw new Error("Bootstrap's JavaScript requir
 		}, index*50+500);
 
 	});
+	} else{
+		this.$eel.html('<div style="transform:translateZ(300px);opacity:0"></div>'
+		+'<div style="transform:translateZ(300px);opacity:0"></div>'
+		+'<div style="transform:translateZ(300px);opacity:0"></div>'
+		+'<div style="transform:translateZ(300px);opacity:0"></div>'
+		+'<div style="transform:translateZ(300px);opacity:0"></div>'
+		+'<div style="transform:translateZ(300px);opacity:0"></div>'
+		+'<div style="transform:translateZ(300px);opacity:0"></div>'
+		+'<div style="transform:translateZ(300px);opacity:0"></div>'
+		+'<div style="transform:translateZ(300px);opacity:0"></div>'
+		+'<div style="transform:translateZ(300px);opacity:0"></div>'
+
+		);
+	
+	this.$eel.find('div').each(function (index,element) {
+		$(this).css('background-position-y',-$('#continer').height()*index/10);
+	});
+		this.imgindex-=1;
+	if (this.imgindex<0) {
+		this.imgindex=this.imgarr.length-1;
+	}
+	this.preimgindex=this.imgindex;
+	if (this.preimgindex<0) {
+		this.preimgindex=this.imgarr.length-1;
+	}
+	
+
+	var self=this;
+		this.$eel.find('div').each(function (index,element) {
+			
+//		$(this).show();
+		
+		var temp=$(this);
+		temp.css('background-image',"url('" + (self.imgarr[self.preimgindex]) +"')");
+		temp.css('visibility','visible');
+
+		
+		
+//		temp.css('transition-property','transform,opacity');
+		setTimeout(function() {
+//			temp.animate({transform:'translateZ(300px)'},'500');
+			self.arr.push(temp);
+			temp.css('transform','translateZ(0px)');
+			console.log(temp);
+			
+			
+		}, index*50);
+		setTimeout(function() {
+//			temp.animate({opacity:'0'},250);
+			temp.css('opacity','1');
+			console.log(temp);
+			
+			
+		}, index*50);
+		setTimeout(function() {
+////			arr.shift();
+//			temp.css('visibility','hidden');
+////			temp.css('transition-property','');
+//			temp.css('opacity','1');
+//			temp.css('transform','');
+//			temp.css('background-image',"url('img/" + imgindex +".jpg')");
+			self.arr.shift();
+			
+		}, index*50+500);
+		
+
+		
+
+	});
+	
+	setTimeout(function() {
+////			arr.shift();
+//			temp.css('visibility','hidden');
+////			temp.css('transition-property','');
+//			temp.css('opacity','1');
+//			temp.css('transform','');
+//			temp.css('background-image',"url('img/" + imgindex +".jpg')");
+			self.$el.css('background-image',"url('" + (self.imgarr[self.imgindex]) +"')");
+			
+		}, 9*50+500);
+		
+	}
+	
+	
 	
 	
 }
